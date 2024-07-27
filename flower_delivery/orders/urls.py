1edb5b4b51b6bd527315957d1cb65434b36dfa34
RS6M5 <163847599+RS6M5@users.noreply.github.com>
2024-07-27 14:07:26 +0300

    add log code

--- flower_delivery/orders/urls.py
+++ flower_delivery/orders/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import product_list
+from .views import product_list, signup, login_view
 
 urlpatterns = [
     path('', product_list, name='product_list'),
+    path('signup/', signup, name='signup'),
+    path('login/', login_view, name='login'),
 ]
--- flower_delivery/orders/views.py
+++ flower_delivery/orders/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import SignUpForm
 from .models import Product
 
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'orders/product_list.html', {'products': products})
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('product_list')
+    else:
+        form = SignUpForm()
+    return render(request, 'orders/signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('product_list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'orders/login.html', {'form': form})
